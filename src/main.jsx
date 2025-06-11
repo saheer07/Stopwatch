450e42882012a62c43f19a0cabea18ac8ad1d460
saheer07 <saheerchungat07@gmail.com>
2025-06-11 15:51:44 +0530

    9th Commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css"; // ✅ Add this
-import { ThemeProvider } from "./Context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+
  // ✅
 
 import "./index.css";
