e25fd1fae210213033c83221f875285f1df5cc39
saheer07 <saheerchungat07@gmail.com>
2025-06-11 16:04:19 +0530

    13th Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
-
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // ✅ Add this
+import "./index.css"; // ✅ Only once
 import { ThemeProvider } from "./Context/ThemeContext.jsx";
-
-
- // ✅
-
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
