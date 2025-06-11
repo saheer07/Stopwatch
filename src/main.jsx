b7a7c896ebeae3b82f7384edb3aeab6d82fe8009
saheer07 <saheerchungat07@gmail.com>
2025-06-11 16:11:20 +0530

    Fix ThemeContext path issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // ✅ Only once
-import { ThemeProvider } from "./Context/ThemeContext.jsx";
+import "./index.css"; 
+import { ThemeProvider } from "./Context/ThemeContext.jsx"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
