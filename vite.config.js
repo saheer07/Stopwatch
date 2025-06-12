f89517cea60b45cb8b291d050c85533bf8a53d7b
saheer07 <saheerchungat07@gmail.com>
2025-06-12 10:29:59 +0530

    7th Commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@Context": "/src/Context", // Ensure this matches your folder structure
+    },
+  },
 })
