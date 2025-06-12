55d63e37b1d8eda7bdd3739b988091cacd57d0c9
saheer07 <saheerchungat07@gmail.com>
2025-06-12 10:25:10 +0530

    Fix ThemeProvider import and usage

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; 
-import { ThemeContext } from "./Context/ThemeContext.jsx";
+import "./index.css";
+import { ThemeProvider } from "./Context/ThemeContext.jsx"; // Import ThemeProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContext>
+    <ThemeProvider> {/* Use ThemeProvider instead of ThemeContext */}
       <App />
-    </ThemeContext>
+    </ThemeProvider>
   </React.StrictMode>
 );
